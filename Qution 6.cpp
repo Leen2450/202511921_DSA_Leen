994be8d011f3a5384bb0dfc1b25b9ae4921e80bc
Leen2450 <aleen2450@gmail.com>
2026-05-14 13:04:58 +0200

    Qution 6.cpp

---
+++ Qution 6.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+// Recursive factorial function
+int factorial(int n)
+{
+    if (n == 0 || n == 1)
+        return 1;
+
+    else
+        return n * factorial(n - 1);
+}
+
+int main()
+{
+    int number;
+
+    cout << "Enter a number: ";
+    cin >> number;
+
+    cout << "Factorial = " << factorial(number);
+
+    return 0;
+}
